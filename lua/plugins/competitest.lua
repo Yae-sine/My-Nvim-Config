3dad55c3a3fc0d37ab48bfcb4f066c4c94f8fcb4
Yassine Aboulhaj <aboulhajyassine@gmail.com>
2026-03-19 00:36:08 +0000

    Added the competitest plugin

---
+++ lua/plugins/competitest.lua
@@ -0,0 +1,13 @@
+return {
+  "xeluxee/competitest.nvim",
+  dependencies = "MunifTanjim/nui.nvim",
+  config = function()
+    require("competitest").setup({
+      testcases_directory = "/home/yassine/Documents/My Data/playground/CP_LEET/Problems/Test_cases",
+      template_file = "/home/yassine/.config/nvim/templates/template.cpp",
+      received_problems_path = "/home/yassine/Documents/My Data/playground/CP_LEET/Problems/$(PROBLEM).$(FEXT)",
+      received_contests_directory = "/home/yassine/Documents/My Data/playground/CP_LEET/Contests/",
+      received_contests_problems_path = "/home/yassine/Documents/My Data/playground/CP_LEET/$(PROBLEM).$(FEXT)",
+    })
+  end,
+}
---
+++ templates/template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// ===================== SOLUTION =====================
+void solve() {
+
+    cout << "\n";
+}
+
+
+
+
+
+
+// ===================== MAIN =====================
+int main() {
+
+    int t = 1;
+    cin >> t; // comment this if single test case
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
